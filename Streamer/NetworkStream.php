1a0d880e0d144b627584cf5b818c373d6553b061
Demin Yin <deminy@deminy.net>
2014-11-25 15:11:37 -0800

    fix method NetwordStream::create()

--- Streamer/NetworkStream.php
+++ Streamer/NetworkStream.php
@@ -6,7 +6,18 @@ class NetworkStream extends Stream
 {
     public static function create($address, $timeout = null, $flags = null, $context = null)
     {
-        return new static(stream_socket_client($address, $errno, $errstr, $timeout, $flags, $context));
+        // stream_socket_client needs to be called in the correct way based on what we have been passed.
+        if (is_null($timeout) && is_null($flags) && is_null($context)) {
+            $fp = stream_socket_client($address, $errno, $errstr);
+        } else if (is_null($flags) && is_null($context)) {
+            $fp = stream_socket_client($address, $errno, $errstr, $timeout);
+        } else if (is_null($context)) {
+            $fp = stream_socket_client($address, $errno, $errstr, $timeout, $flags);
+        } else {
+            $fp = stream_socket_client($address, $errno, $errstr, $timeout, $flags, $context);
+        }
+
+        return new static($fp);
     }
 
     public function getName($remote = true)
